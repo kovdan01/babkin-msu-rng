481537fd3e7ffcf1f725e2d04adc4d262342f590
Daniil Kovalev <kovdan01@yandex.ru>
2020-03-02 23:40:59 +0300

    Add parsing command-line arguments with boost program options

--- check-quality/main.cpp
+++ check-quality/main.cpp
@@ -1,33 +1,33 @@
 #include "babkin.h"
 #include "utils.h"
+
 #include <gmpxx.h>
+#include <boost/program_options.hpp>
+
 #include <iostream>
 #include <vector>
-#include <random>
 #include <iomanip>
 #include <algorithm>
 #include <chrono>
 #include <map>
+#include <stdexcept>
 
-using BabkinLong = Babkin<mpz_class>;
-
-int main()
+template <typename Integer>
+void test(std::size_t block_size, std::size_t blocks_count)
 {
-    std::size_t block_size = (1 << 12);
-    std::cout << "Start babkin creation...\n";
-    BabkinLong babkin(block_size);
-    std::cout << "Finish babkin creation!\n";
+    std::cout << "Start babkin creation..." << std::endl;
+    Babkin<Integer> babkin(block_size);
+    std::cout << "Finish babkin creation!" << std::endl;
 
-    std::cout << "Start generating blocks...\n";
-    std::map<int, std::vector<BabkinLong::NBlock>> percentage_to_blocks;
+    std::cout << "Start generating blocks..." << std::endl;
+    std::map<int, std::vector<typename Babkin<Integer>::NBlock>> percentage_to_blocks;
     for (int percentage = 10; percentage < 100; percentage += 10)
     {
-        std::size_t blocks_count = 1'000;
-        percentage_to_blocks[percentage] = std::vector<BabkinLong::NBlock>(blocks_count);
-        for (BabkinLong::NBlock& block : percentage_to_blocks[percentage])
+        percentage_to_blocks[percentage] = std::vector<typename Babkin<Integer>::NBlock>(blocks_count);
+        for (typename Babkin<Integer>::NBlock& block : percentage_to_blocks[percentage])
             block = generate_block<mpz_class>(block_size, percentage / 100.0);
     }
-    std::cout << "Finish generating blocks\n";
+    std::cout << "Finish generating blocks" << std::endl;
 
     for (auto& [percentage, blocks] : percentage_to_blocks)
     {
@@ -35,14 +35,14 @@ int main()
         using namespace std::chrono;
         time_point<high_resolution_clock> begin, end;
         std::int64_t time = 0;
-        for (const BabkinLong::NBlock& block : blocks)
+        for (const typename Babkin<Integer>::NBlock& block : blocks)
         {
             begin = high_resolution_clock::now();
-            BabkinLong::CodeBlock code_block = babkin.get_random_block(block);
+            typename Babkin<Integer>::CodeBlock code_block = babkin.get_random_block(block);
             end = high_resolution_clock::now();
             time += duration_cast<nanoseconds>(end - begin).count();
 #ifndef NDEBUG
-            BabkinLong::CodePair code_pair = babkin.encode(block);
+            Babkin<Integer>::CodePair code_pair = babkin.encode(block);
             std::cerr << "     " << block << "\n --> " << code_pair << "\n --> " << code_block << '\n';
 #endif
             std::copy(code_block.begin(), code_block.end(), std::back_inserter(cumulative));
@@ -54,6 +54,59 @@ int main()
         std::cout << percentage << "% of ones: "
                   << "output ones proportion is "    << std::setw(8) << ones_proportion << ", "
                   << "output speed is "              << std::setw(7) << output_speed << " Mbit/s, "
-                  << "remaining data proportion is " << std::setw(8) << remaining_data_proportion << '\n';
+                  << "remaining data proportion is " << std::setw(8) << remaining_data_proportion << std::endl;
     }
+}
+
+int main(int argc, char* argv[]) try
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    namespace po = boost::program_options;
+    po::options_description desc("Allowed options");
+    desc.add_options()
+        ("help,H",                                                  "Print this message")
+        ("size,S",  po::value<std::size_t>()->default_value(64),    "Block size for the input of Babkin's algorithm. "
+                                                                    "Long arithmetics will be used if it is more than 64, "
+                                                                    "that will lead to perfomance decrease")
+        ("count,C", po::value<std::size_t>()->default_value(1'000), "Number of blocks to be generated and processed")
+        ;
+
+    po::variables_map vm;
+    try
+    {
+        po::store(parse_command_line(argc, argv, desc), vm);
+        if (vm.contains("help"))
+        {
+            std::cout << "This program checks the following characteristics of the alogrithm:\n"
+                         " - proportion of '1' in output sequence\n"
+                         " - output sequence generation speed\n"
+                         " - proportion of remaining data (the ratio of input and output sequences length)\n"
+                         "All checks are made for input sequences of bits that have Bernoulli distribution\n"
+                         "with parameters (0.1, 0.9), (0.2, 0.8), ..., (0.9, 0.1).\n\n"
+                      << desc << '\n';
+            return 0;
+        }
+        po::notify(vm);
+    }
+    catch (const po::error& error)
+    {
+        std::cerr << "Error while parsing command-line arguments: "
+                  << error.what() << "\nPlease use --help to see help message\n";
+        return 1;
+    }
+
+    std::size_t block_size = vm["size"].as<std::size_t>();
+    std::size_t blocks_count = vm["count"].as<std::size_t>();
+
+    if (block_size > 64)
+        test<mpz_class>(block_size, blocks_count);
+    else
+        test<std::uint64_t>(block_size, blocks_count);
+}
+catch (const std::exception& e)
+{
+    std::cerr << e.what() << '\n';
+    return 1;
 }
--- generate-data/main.cpp
+++ generate-data/main.cpp
@@ -1,39 +1,92 @@
 #include "babkin.h"
 #include "utils.h"
+
 #include <gmpxx.h>
+#include <boost/program_options.hpp>
+
 #include <iostream>
 #include <vector>
-#include <random>
-#include <iomanip>
 #include <algorithm>
-#include <chrono>
-#include <map>
+#include <stdexcept>
 
-using BabkinLong = Babkin<mpz_class>;
-
-int main()
+template <typename Integer>
+void job(std::size_t block_size, std::size_t blocks_count, double probability, const std::string& output_filename)
 {
-    std::size_t block_size = (1 << 12);
-    std::cout << "Start babkin creation...\n";
-    BabkinLong babkin(block_size);
-    std::cout << "Finish babkin creation!\n";
+    std::cout << "Start babkin creation..." << std::endl;
+    Babkin<Integer> babkin(block_size);
+    std::cout << "Finish babkin creation!" << std::endl;
 
-    int percentage = 20;
-    std::size_t blocks_count = 10'000;
-
-    std::cout << "Start generating data...\n";
+    std::cout << "Start generating data..." << std::endl;
     std::vector<std::uint8_t> cumulative;
     for (std::size_t i = 0; i < blocks_count; ++i)
     {
-        if (i % 1'000 == 0)
-            std::cout << "Generating data from block " << i << "...\n";
-        BabkinLong::NBlock block = generate_block<mpz_class>(block_size, percentage / 100.0);
-        BabkinLong::CodeBlock code_block = babkin.get_random_block(block);
+        if (i % 1'000 == 0 || i + 1 == blocks_count)
+            std::cout << "Generating data from block " << i << "..." << std::endl;
+        typename Babkin<Integer>::NBlock block = generate_block<mpz_class>(block_size, probability);
+        typename Babkin<Integer>::CodeBlock code_block = babkin.get_random_block(block);
         std::copy(code_block.begin(), code_block.end(), std::back_inserter(cumulative));
     }
-    std::cout << "Finish generating data\n";
+    std::cout << "Finish generating data" << std::endl;
 
-    std::cout << "Start writing to file...\n";
-    write_to_file(compress_bits(cumulative), "data.bin");
-    std::cout << "Finish writing to file\n";
+    std::cout << "Start writing to file..." << std::endl;
+    write_bytes_to_file(compress_bits(cumulative), output_filename);
+    std::cout << "Finish writing to file" << std::endl;
+}
+
+int main(int argc, char* argv[]) try
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    namespace po = boost::program_options;
+    po::options_description desc("Allowed options");
+    desc.add_options()
+        ("help,H",                                                        "Print this message")
+        ("size,S",        po::value<std::size_t>()->default_value(64),    "Block size for the input of Babkin's algorithm. "
+                                                                          "Long arithmetics will be used if it is more than 64, "
+                                                                          "that will lead to perfomance decrease")
+        ("count,C",       po::value<std::size_t>()->default_value(1'000), "Number of blocks to be generated and processed")
+        ("probability,P", po::value<double>()->default_value(0.5),        "Parameter of Bernoulli distribution "
+                                                                          "proportion of '1' in bit sequence")
+        ("output,O",      po::value<std::string>()->required(),           "Name of output file")
+        ;
+
+    po::variables_map vm;
+    try
+    {
+        po::store(parse_command_line(argc, argv, desc), vm);
+        if (vm.contains("help"))
+        {
+            std::cout << "This program generates data to be tested with NIST statistical test suite.\n"
+                         "Steps:\n"
+                         " - The program generates bit sequence with Bernoulli distribuion.\n"
+                         "   The sequence contains <count> bit blocks of <size> length.\n"
+                         " - Then this sequence is treated as an input for Babkin's algorithm,\n"
+                         "   and its output is stored to <output> file\n\n"
+                      << desc << '\n';
+            return 0;
+        }
+        po::notify(vm);
+    }
+    catch (const po::error& error)
+    {
+        std::cerr << "Error while parsing command-line arguments: "
+                  << error.what() << "\nPlease use --help to see help message\n";
+        return 1;
+    }
+
+    std::size_t block_size = vm["size"].as<std::size_t>();
+    std::size_t blocks_count = vm["count"].as<std::size_t>();
+    double probability = vm["probability"].as<double>();
+    std::string output_filename = vm["output"].as<std::string>();
+
+    if (block_size > 64)
+        job<mpz_class>(block_size, blocks_count, probability, output_filename);
+    else
+        job<std::uint64_t>(block_size, blocks_count, probability, output_filename);
+}
+catch (const std::exception& e)
+{
+    std::cerr << e.what() << '\n';
+    return 1;
 }
